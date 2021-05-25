bf16aef4b11b7fd80928a1b7592038f1d4412776
Fabricio-Lobo <fabriciolobo62@gmail.com>
2021-05-24 22:47:18 -0600

    Deploy App

---
+++ build/precache-manifest.5695d876b1245a1ef12c3317b5a3ae2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d93727b93b4592b773074b9bfec26c6",
+    "url": "/React-Project-Udemy-1/index.html"
+  },
+  {
+    "revision": "bc8ee740e191e6704a9c",
+    "url": "/React-Project-Udemy-1/static/css/main.a142fb8a.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/React-Project-Udemy-1/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Project-Udemy-1/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc8ee740e191e6704a9c",
+    "url": "/React-Project-Udemy-1/static/js/main.db1bb081.chunk.js"
+  },
+  {
+    "revision": "3ce07fd92cc3822e2aa6",
+    "url": "/React-Project-Udemy-1/static/js/runtime-main.80a2a243.js"
+  }
+]);
